25877b1330f6301ad213e6747344407355c3b3d6
michaeleahcim <somethingnobodywillthinkof@gmail.com>
2015-07-28 06:29:37 -0400

    Fixed schedule page

--- schedule.js
+++ schedule.js
@@ -2,50 +2,70 @@
 var work = $( ".work" );
 var personal = $( ".personal" );
 var sleep = $( ".sleep" );
-var graycolor = "#2D2D2D";
 
 var workhours = $( "#workhours" );
 var personalhours = $( "#personalhours" );
 var sleephours = $( "#sleephours" );
 
+/*
 var bgGreen = "#366056";
 var bgRed = "#6B3D3F";
 var bgPurple = "#79688F";
+var graycolor = "#2D2D2D";
+
+*/
 
 workhours.mouseenter(function() {
-	personal.css( "background", graycolor );
-	sleep.css( "background", graycolor );
+	personal.css( "opacity", 0.4 );
+	sleep.css( "opacity", 0.4 );
 });
 
 workhours.mouseleave(function() {
-	personal.css( "background", bgGreen);
-	sleep.css( "background", bgPurple );
+	personal.css( "opacity", 1);
+	sleep.css( "opacity", 1);
 });
 
 
 personalhours.mouseenter(function() {
-  	work.css( "background", graycolor );
-	sleep.css( "background", graycolor );
+  	work.css( "opacity", 0.4 );
+	sleep.css( "opacity", 0.4 );
 });
 
 personalhours.mouseleave(function() {
-	work.css( "background", bgRed );
-	sleep.css( "background", bgPurple );
+	work.css( "opacity", 1);
+	sleep.css( "opacity", 1);
 });
 
 
 sleephours.mouseenter(function() {
-  	work.css( "background", graycolor );
-	personal.css( "background", graycolor );
+  	work.css( "opacity", 0.4 );
+	personal.css( "opacity", 0.4 );
 });
 
 sleephours.mouseleave(function() {
- 	work.css( "background", bgRed );
-	personal.css( "background", bgGreen );
+ 	work.css( "opacity", 1);
+	personal.css( "opacity", 1);
 });
 
-var defaultmessage = 'On weekends I take care of cleaning, laundry then do whatever I want.';
 
+
+
+/*
+
+BOXES
+
+*/
+
+//var defaultmessage = 'On weekends I take care of cleaning, laundry then do whatever I want.';
+$( "#boxes #box" ).mouseenter(function() {
+	$(this).css( "opacity", 0.4 );
+});
+
+$( "#boxes #box" ).mouseleave(function() {
+	$(this).css( "opacity", 1);
+});
+
+/*
 $( "#boxes #box:nth-child(1)" ).mouseenter(function() {
 	$( ".text" ).text('Wake up go straight to shower and get ready for work. Make Coffee.');
 });
@@ -116,11 +136,9 @@ $( "#boxes #box:nth-child(16)" ).mouseenter(function() {
 $( "#boxes #box:nth-child(16)" ).mouseleave(function() {
 	$( ".text" ).text(defaultmessage);
 });
-
-/*
-.work	background: #6B3D3F;
-
-.sleep	background: #79688F;
-
-.personal	background: #366056
 */
+
+
+
+
+
