6ebfcce050b8cbfaf07d5eb8ce5f20e5717583f3
michaeleahcim <somethingnobodywillthinkof@gmail.com>
2015-08-04 21:55:04 -0400

    Added px size type

--- infinite.js
+++ infinite.js
@@ -204,7 +204,7 @@ var tumblrAutoPager = {
         	alert("message");
         	var width = document.body.clientWidth;
         	var newWidth = width + 500;
-        	document.body.style.width=newWidth;
+        	document.body.style.width=newWidth+"px";
 
             if (oj.status == 404) {
                 tAP.remainFlg = false;
